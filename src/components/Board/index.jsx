aff3c609f6733a23fbfc10becb837f044102260c
LeoSandrini <thesandrini@gmail.com>
2021-04-19 02:59:40 -0300

    Adding moves control and winner determination

--- src/components/Board/index.jsx
+++ src/components/Board/index.jsx
@@ -2,18 +2,45 @@ import React, {useState} from 'react';
 import './index.css';
 
 function Board() {
-    const [values, setValues] = useState(Array(9).fill(null))
+    const [squares, setSquares] = useState(Array(9).fill(null))
+	const [xIsNext, setXIsNext] = useState(true)
+	const [moves, setMoves] = useState(0)
+
+	const determineWinner = (squares) => {
+		const lines = [
+			[0, 1, 2],
+			[3, 4, 5],
+			[6, 7, 8],
+			[0, 3, 6],
+			[1, 4, 7],
+			[2, 5, 8],
+			[0, 4, 8],
+			[2, 4, 6],
+		  ]
+		for(let i=0; i < lines.length; i++){
+			let [a, b, c] = lines[i]
+			if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
+				return squares[a]
+			}
+		}
+		return null
+	}
 
 	const handleClick = (i) => {
-		const squares = values.slice()
-		squares[i] = 'X'
-		setValues(squares)
+		//Makes a copy of the board in order to maintain immutability
+		const square = squares.slice()
+		//Checks if the game is over or if the square is already filled
+		if( winner || square[i] ) return
+		square[i] = xIsNext ? 'X' : 'O'
+		setMoves(prevMoves => prevMoves + 1)
+		setSquares(square)
+		setXIsNext(!xIsNext)
 	}
 
     const renderSquare = (i) => {
 		return ( 
 		<Square 
-		value={values[i]}
+		value={squares[i]}
 		onClick={() => handleClick(i)}/>
 		)
 	};
@@ -21,13 +48,15 @@ function Board() {
     const Square = (props) => {
         return (
         <button className='square' 
-		onClick={() => props.onClick}>
+		onClick={props.onClick}>
             {props.value}
         </button>
         )
     }
 
-	const status = 'Next player: X';
+	let winner =  determineWinner(squares)
+	let status = winner ? `Winner: ${winner}` : `Next player: ${xIsNext ? 'X' : 'O'}`
+	if(!winner && moves === 9) status = 'Draw'
 
 	return (
 		<div>
@@ -38,8 +67,8 @@ function Board() {
 				{renderSquare(2)}
 			</div>
 			<div className='board-row'>
-				{renderSquare(4)}
 				{renderSquare(3)}
+				{renderSquare(4)}
 				{renderSquare(5)}
 			</div>
 			<div className='board-row'>
