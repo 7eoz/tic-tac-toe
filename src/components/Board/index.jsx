09289043608d0f11068890f7d1f93cff90b9bff1
Leonardo Sandrini <thesandrini@gmail.com>
2021-05-17 23:47:50 -0300

    Update index.jsx
    retrieving version

--- src/components/Board/index.jsx
+++ src/components/Board/index.jsx
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import './index.css';
 
+<<<<<<< HEAD
 function Game() {
 	const [squares, setSquares] = useState(Array(9).fill(null))
 	const [history, setHistory] = useState(useState(Array(9).fill(null)))
@@ -88,6 +89,53 @@ function Game() {
 					{renderSquare(7)}
 					{renderSquare(8)}
 				</div>
+=======
+function Board() {
+    const [values, setValues] = useState(Array(9).fill(null))
+
+	const handleClick = (i) => {
+		const squares = values.slice()
+		squares[i] = 'X'
+		setValues(squares)
+	}
+
+    const renderSquare = (i) => {
+		return ( 
+		<Square 
+		value={values[i]}
+		onClick={() => handleClick(i)}/>
+		)
+	};
+
+    const Square = (props) => {
+        return (
+        <button className='square' 
+		onClick={() => props.onClick}>
+            {props.value}
+        </button>
+        )
+    }
+
+	const status = 'Next player: X';
+
+	return (
+		<div>
+			<div className='status'>{status}</div>
+			<div className='board-row'>
+				{renderSquare(0)}
+				{renderSquare(1)}
+				{renderSquare(2)}
+			</div>
+			<div className='board-row'>
+				{renderSquare(4)}
+				{renderSquare(3)}
+				{renderSquare(5)}
+			</div>
+			<div className='board-row'>
+				{renderSquare(6)}
+				{renderSquare(7)}
+				{renderSquare(8)}
+>>>>>>> parent of aff3c60 (Adding moves control and winner determination)
 			</div>
 		);
 	}
