e7f04d87fe11bcd9d73b30ea1bd2b2f4d6dc4771
Leonardo Sandrini <thesandrini@gmail.com>
2021-05-19 02:53:18 -0300

    errors fixed

--- src/components/Board/index.jsx
+++ src/components/Board/index.jsx
@@ -1,13 +1,10 @@
 import React, {useState} from 'react';
 import './index.css';
 
-function Game() {
-	const [squares, setSquares] = useState(Array(9).fill(null))
-	const [history, setHistory] = useState(useState(Array(9).fill(null)))
+function Board() {
+    const [clickedSquares, setClickedSquares] = useState(Array(9).fill(null))
 	const [xIsNext, setXIsNext] = useState(true)
 	const [moves, setMoves] = useState(0)
-
-	const current = history[history.length - 1]	
 
 	const determineWinner = (squares) => {
 		const lines = [
@@ -29,86 +26,72 @@ function Game() {
 		return null
 	}
 
-	//Determine Status of the game
-	let winner =  determineWinner(squares)
+	const handleClick = (i) => {
+	//Makes a copy of the board in order to maintain immutability
+	const squares = clickedSquares.slice()
+	//Checks if the game is over or if the square is already filled
+	if( winner || squares[i] ) return
+	squares[i] = xIsNext ? 'X' : 'O'
+	setMoves(prevMoves => prevMoves + 1)
+	setClickedSquares(squares)
+	setXIsNext(!xIsNext)
+	}
+
+    const renderSquare = (i) => {
+		return ( 
+		<Square 
+		value={clickedSquares[i]}
+		onClick={() => handleClick(i)}/>
+		)
+	};
+
+    const Square = (props) => {
+        return (
+        <button className='square' 
+		onClick={props.onClick}>
+            {props.value}
+        </button>
+        )
+    }
+
+	let winner =  determineWinner(clickedSquares)
 	let status = winner ? `Winner: ${winner}` : `Next player: ${xIsNext ? 'X' : 'O'}`
 	if(!winner && moves === 9) status = 'Draw'
 
-
-
-	function Board(props) {
-
-		const handleClick = (i) => {
-			//Makes a copy of the board in order to maintain immutability
-			const square = squares.slice()
-			setHistory(...history, squares)
-			//Checks if the game is over or if the square is already filled
-			if( winner || square[i] ) return
-	
-			square[i] = xIsNext ? 'X' : 'O'
-			setMoves(prevMoves => prevMoves + 1)
-			setSquares(square)
-			setXIsNext(!xIsNext)
-		}
-
-		const renderSquare = (i) => {
-			return ( 
-			<Square 
-			value={props.squares[i]}
-			onClick={() => props.handleClick(i)}/>
-			)
-		};
-
-		const Square = (props) => {
-			return (
-			<button className='square' 
-			onClick={props.onClick}>
-				{props.value}
-			</button>
-			)
-		}
-
-
-
-		return (
-			<div>
-				<div className='status'>{status}</div>
-				<div className='board-row'>
-					{renderSquare(0)}
-					{renderSquare(1)}
-					{renderSquare(2)}
-				</div>
-				<div className='board-row'>
-					{renderSquare(3)}
-					{renderSquare(4)}
-					{renderSquare(5)}
-				</div>
-				<div className='board-row'>
-					{renderSquare(6)}
-					{renderSquare(7)}
-					{renderSquare(8)}
-				</div>
-			</div>
-		);
-	}
-
-
-
 	return (
-		<div className='game'>
-			<div className='game-board'>
-				<Board
-				squares={current.squares}
-				onClick={(i) => this.handleClick(i)} />
+		<div>
+			<div className='status'>{status}</div>
+			<div className='board-row'>
+				{renderSquare(0)}
+				{renderSquare(1)}
+				{renderSquare(2)}
 			</div>
-			<div className='game-info'>
-				<div>{status}</div>
-				<ol>{/* TODO */}</ol>
+			<div className='board-row'>
+				{renderSquare(4)}
+				{renderSquare(3)}
+				{renderSquare(5)}
+			</div>
+			<div className='board-row'>
+				{renderSquare(6)}
+				{renderSquare(7)}
+				{renderSquare(8)}
 			</div>
 		</div>
 	);
 }
 
-// ========================================
+function Game() {
+	return (
+		<div className='game'>
+			<div className='game-board'>
+				<Board />
+			</div>
+			<div className='game-info'>
+				<div>{/* status */}</div>
+				<ol>{/* TODO */}</ol>
+			</div>
+		</div>
+	);
+}
 
 export default Game;
